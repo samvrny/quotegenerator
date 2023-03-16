3af089485065b198f329ef0a7dafed369371abc1
samvrny <samvrny@gmail.com>
2023-03-16 12:30:54 -0500

    got quotes printing to the page

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+const quoteBox = document.getElementById('quote-box');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author-name')
+const quoteButton = document.getElementById('generate-quote');
+const twitterButton = document.getElementById('twitter');
 let quotes = []
 
 //get quotes from API
@@ -7,25 +12,36 @@ async function fetchQuotes() {
     try {
         const response = await fetch(quoteApiUrl);
         quotes = await response.json();
-        console.log(quotes)
         singleQuote();
     } catch (error) {
         //error here
     }
 }
 
-//pick a single quote from
+//pick a single quote from the data to print to the screen
 function singleQuote() {
+    //randomize a quote out of the returned data
     const quoteToUse = quotes[Math.floor(Math.random() * quotes.length)];
-    console.log(quoteToUse)
-}
 
-//idea for other buttons to use:
-//make a const for each tag (let quotes1array = )
-// let quotes1 = []
-// for(i=0; i < 100; i++) {
-//     quotes1.push(quotes[i])
-// }
-// console.log(quotes1)
+    //check to see if the quote and author exist, and if so print them to the screen. 
+    if (!quoteToUse.author) {
+        authorText.textContent = 'Unknown'
+    } else {
+        authorText.textContent = quoteToUse.author;
+    }
+
+    if (!quoteToUse.text) {
+        quoteText.textContent = 'Somethings not working here! Please try again later.'
+    } else {
+        quoteText.textContent = quoteToUse.text;
+    }
+
+    //check the quote's length to determine text-size
+    if(quoteToUse.text.length > 125) {
+        quoteText.classList.add('long-quote')
+    } else {
+        quoteText.classList.remove('long-quote')
+    }
+}
 
 fetchQuotes();
