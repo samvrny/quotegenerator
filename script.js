a6f79db42f04aa1efb7ee27a37c092851415d2bd
samvrny <samvrny@gmail.com>
2023-03-16 22:57:48 -0500

    added loaded to UI so that if things are taking longer on page load the user sees a loader

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
-const quoteBox = document.getElementById('quote-box');
+const quoteBox = document.getElementById('container');
 const quoteText = document.getElementById('quote');
 const authorText = document.getElementById('author-name')
 const quoteButton = document.getElementById('generate-quote');
 const twitterButton = document.getElementById('twitter');
+const loader = document.getElementById('loader');
 let quotes = []
+
+//loading and finishing loading
+function loading() {
+    loader.hidden = false;
+    quoteBox.hidden = true;
+}
+
+function doneLoading() {
+    loader.hidden = true;
+    quoteBox.hidden = false;
+}
 
 //get quotes from API
 async function fetchQuotes() {
+    loading();
     const quoteApiUrl = 'https://jacintodesign.github.io/quotes-api/data/quotes.json';
 
     try {
@@ -20,6 +33,7 @@ async function fetchQuotes() {
 
 //pick a single quote from the data to print to the screen
 function singleQuote() {
+    loading();
     //randomize a quote out of the returned data
     const quoteToUse = quotes[Math.floor(Math.random() * quotes.length)];
 
@@ -32,6 +46,7 @@ function singleQuote() {
 
     if (!quoteToUse.text) {
         quoteText.textContent = 'Somethings not working here! Please try again later.'
+        authorText.textContent = ''
     } else {
         quoteText.textContent = quoteToUse.text;
     }
@@ -42,6 +57,7 @@ function singleQuote() {
     } else {
         quoteText.classList.remove('long-quote')
     }
+    doneLoading();
 }
 
 //post to twitter
